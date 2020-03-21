e03ae08921f96a45d01c88c838c2c208ea30cc0f
BS <bs@bss-mac-pro.home>
2020-03-21 19:19:40 +0100

    Add main folder

---
+++ VFire/src/firebase.js
@@ -0,0 +1,11 @@
+import {initializeApp} from 'firebase';
+
+const app = initializeApp({
+	apiKey: "AIzaSyBfj-x0qc7sfihGkiEDCeBm69y_0-Edtsw",
+	authDomain: "vue-vfire-project.firebaseapp.com",
+	databaseURL: "https://vue-vfire-project.firebaseio.com",
+	projectId: "vue-vfire-project",
+	storageBucket: "vue-vfire-project.appspot.com",
+	messagingSenderId: "10473019536",
+	appId: "1:10473019536:web:c4bca438a833ba1e686015"
+})
